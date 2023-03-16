b5e6e77600431315e63b0f4a57e6156ce39521ad
Pseurae <adhith.chand@gmail.com>
2023-03-16 11:54:42 +0530

    Replace the TM31 in the same space instead of repointing

--- scripts/bugfixes/item_desc.asm
+++ scripts/bugfixes/item_desc.asm
@@ -1,17 +1,18 @@
 ; Override item names from table
-.macro fix_item_desc, i, desc
+.macro replace_item_desc, i, desc
 .org 0x83c5564 + (44 * i) + 20
 .area 0x4, 0x0
 .word desc
 .endarea
 .endmacro
 
-.if ITEM_DESCRIPTIONS
-fix_item_desc ITEM_TM31_ICE_PUNCH, @@IcePunch_Desc
+@TM31_IcePunch_Desc equ 0x83c4fa4
+@TM31_IcePunch_DescEnd equ 0x83c4fdb
 
+.if ITEM_DESCRIPTIONS
 ; Description Strings
-.autoregion
-@@IcePunch_Desc:
+.org @TM31_IcePunch_Desc
+.area @TM31_IcePunch_DescEnd - @TM31_IcePunch_Desc, 0x0
 .string "A freezing punch\nthat can leave the\nfoe frozen solid."
-.endautoregion
+.endarea
 .endif
