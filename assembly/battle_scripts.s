cb2858c570d6d51155e0c9ad2b927844b1b39f4b
Pseurae <adhith.chand@gmail.com>
2024-01-31 00:48:24 +0530

    Move Exp on capture code to assembly folder

---
+++ assembly/battle_scripts.s
@@ -0,0 +1,18 @@
+.include "include/constants/vars.h"
+.include "include/constants/flags.h"
+.include "include/constants/items.h"
+.include "include/constants/species.h"
+.include "include/constants/event_objects.h"
+.include "include/constants/items.h"
+.include "include/constants/species.h"
+.include "include/constants/moves.h"
+.include "include/constants/field_effects.h"
+.include "include/constants/songs.h"
+.include "include/constants/heal_locations.h"
+
+.include "assembly/macros/constants.inc"
+.include "assembly/macros/battle_script.inc"
+
+.section .script_data, "aw", %progbits
+
+.include "assembly/battle/exp_on_capture.inc"
