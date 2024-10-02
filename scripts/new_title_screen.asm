8430dd6f09f1142cd74ad48fded3d23e8e8f711e
Pseurae <adhith.chand@gmail.com>
2024-10-02 04:18:58 +0000

    Add press start graphic

--- scripts/new_title_screen.asm
+++ scripts/new_title_screen.asm
@@ -3,4 +3,13 @@
 .area 0x654, 0x0
 .import "assets/logo.8bpp.lz"
 .endarea
+
+.org 0x8e9d644
+.autoregion
+@PressStart:
+.import "assets/press_start.4bpp.lz"
+.endautoregion
+
+.org 0x8393F8C
+.word @PressStart
 .endif
