31df7b31fd1a11afeab0e2636bb86ea93c16bd94
Pseurae <adhith.chand@gmail.com>
2025-12-29 20:53:40 +0530

    Add decapped item descriptions

--- pyscripts/extract_decap_strings.py
+++ pyscripts/extract_decap_strings.py
@@ -9,7 +9,7 @@ def decapitalize(s: str):
 
 def process_single_string(f, offset: int, length: int=0xff):
     f.seek(offset)
-    return decapitalize(charmap.decode(f.read(length)))
+    return charmap.decode(f.read(length))
 
 def process_string_array(f, offset: int, length: int = 0xff, count: int = 0xff, stride: int = None):
     f.seek(offset)
@@ -24,12 +24,12 @@ def process_string_pointer_array(f, offset: int, count: int, stride: int):
     for _ in range(count):
         f.seek(currentOffset)
         ptr: int = unpack(r"<I", f.read(4))[0] - 0x8000000
-        yield process_single_string(f, ptr)
+        yield (ptr, process_single_string(f, ptr))
         currentOffset += stride
     
 if __name__ == "__main__":
     charmap.parseCharmap("charmap.txt")
     f = open("snakewood_decapped.gba", "rb")
 
-    for i, name in enumerate(process_string_array(f, 0x3c5564, 14, 349, 44)):
-        print(f"@decap_item_name {items.names[i]}, \"{name}\"")
+    for i, (ptr, name) in enumerate(process_string_pointer_array(f, 0x3c5564 + 20, 349, 44)):
+        print(f"@decap_item_desc {items.names[i]}, {hex(0x8000000 + ptr)}, \"{name}\"")
--- scripts/decap.asm
+++ scripts/decap.asm
@@ -1,4 +1,5 @@
 .include "scripts/decap/ability_names.asm"
+.include "scripts/decap/item_descriptions.asm"
 .include "scripts/decap/item_names.asm"
 .include "scripts/decap/species.asm"
 .include "scripts/decap/trainer_class_names.asm"
